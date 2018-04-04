2ae886483627c354c82c4184510bee4ba56c8d64
Tom Cheng <czq@bu.edu>
2018-04-04 01:56:39 -0400

    missing last step

--- src/sessionization.cpp
+++ src/sessionization.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
+#include <cassert>
 #include "date.h"
 
 //TODO instead of update dict and L for every line
@@ -25,23 +26,27 @@ struct userRecord {
 int main(int argc, char *argv[])
 {
 	// check arguments
-	if (argc != 4) {
-		cout << "Insufficient arguments!" << endl;
-		exit(EXIT_FAILURE);
-	}
+	//if (argc != 4) {
+	//	cout << "Insufficient arguments!" << endl;
+	//	exit(EXIT_FAILURE);
+	//}
 
 	unordered_map<string, userRecord> dict;
 	list<pair<string, sys_seconds>> L; //<ip, time>
+	sys_seconds prev;
 
 	//read inactivity period value
-	ifstream period_file (argv[2]);
+	//ifstream period_file (argv[2]);
+	ifstream period_file ("./input/inactivity_period.txt");
 	string period_s;
 	getline(period_file, period_s);
-	int period = atoi(period_s);
+	unsigned int period = stoi(period_s);
+	unsigned int period_ms = 1000*period;
 	cout << "period: " << period << endl;
 
 	// open input log file
-	ifstream infile (argv[1]);
+	//ifstream infile (argv[1]);
+	ifstream infile ("./input/log.csv");
 	string line;
 
 	// skip the first line
@@ -66,34 +71,55 @@ int main(int argc, char *argv[])
 		istringstream dt (datetime);
 		sys_seconds tp;
 		dt >> parse("%F,%T", tp);
-		cout << tp << endl;
+		//cout << tp << endl;
+
+	//	auto diff = std::chrono::duration_cast<std::chrono::milliseconds> (tp-prev);
+	//	unsigned int diff_ms = diff.count();
+
+	//	cout << diff_ms << "ms" << endl;
+	//	prev = tp;
 
 		//clean inactive users
-
-
-
-
+		while (L.size()) {
+			auto l = L.back();
+			auto it = dict.find(l.first);
+			assert(it != dict.end());
+			auto record = it->second;
+			auto diff = std::chrono::duration_cast<std::chrono::milliseconds> (tp - record.end);
+			unsigned int diff_ms = diff.count();
+			if (diff_ms > period_ms) {
+				cout << "printing to file: " << l.first << " " << record.count << endl;
+				dict.erase(l.first);
+				L.pop_back();
+			} else
+				break;
+		}
 
 		//update active users
-		if (M.find(ip) == M.end()) {
+		auto it = dict.find(ip);
+		if (it == dict.end()) {
 			L.push_front({ip, tp});
-			M[ip] = {tp, L.begin()};
+			dict.insert(make_pair(ip, userRecord{tp, L.begin()}));
 		} else {
+			auto& record = it->second;
 			//already exists
-			if (tp == M[ip].end) {
+			if (tp == record.end) {
 				//multiple requests at the same time
 				//just update the count
-				M[ip].count += 1;
+				record.count += 1;
 			} else {
 				//end time needs to be updated
-				L.erase(M[ip].it);
+				L.erase(record.it);
 				L.push_front({ip, tp});
-				M[ip].end = tp;
-				M[ip].count += 1;
-				M[ip].it = L.begin();
+				record.end = tp;
+				record.count += 1;
+				record.it = L.begin();
 			}
 		}
 	}
+
+	//before finish simulation, print all remaining active users
+	//in the ascending order of start time
 }
 
 
